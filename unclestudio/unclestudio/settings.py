7461593fd2dfa16b36c4547c035b8a156e2c2ffd
Samizzo <samizzographics@gmail.com>
2020-01-28 00:10:51 +0300

    commit

--- unclestudio/unclestudio/settings.py
+++ unclestudio/unclestudio/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.AppConfig.home'
+    'home.apps.HomeConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'unclestudio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['unclestudio/TEMPLATES'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
