1203d0b7e461f26ad01ca35f2bbeca45c658a4fc
Samizzo <samizzographics@gmail.com>
2020-01-27 22:16:54 +0300

    Jan 28 commits

--- unclestudio/home/views.py
+++ unclestudio/home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import Http404
 # Create your views here.
+def index(request):
+    name = 'UNCLE S STUDIO WORLD'
+    return render('request', './TEMPLATES/index.html', {'page': name})
