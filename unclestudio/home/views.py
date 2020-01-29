5b49450102c5e69dc2dea50411d9c223a573d658
Samizzo <samizzographics@gmail.com>
2020-01-29 09:58:24 +0300

    commmit

--- unclestudio/home/views.py
+++ unclestudio/home/views.py
@@ -3,4 +3,4 @@ from django.http import Http404
 # Create your views here.
 def index(request):
     name = 'UNCLE S STUDIO WORLD'
-    return render('request', './TEMPLATES/index.html', {'page': name})
+    return render('request', 'index.html', {'page': name})
