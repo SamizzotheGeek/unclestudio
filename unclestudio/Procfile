82358621798b7d17c7e7630424e7dca934324825
Samizzo <samizzographics@gmail.com>
2020-03-27 07:06:20 +0300

    tracking

---
+++ unclestudio/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn unclestudio.wsgi --log-file -
--- unclestudio/unclestudio/settings.py
+++ unclestudio/unclestudio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!a*_@vq^r#w%elbspmzhv5@k0*n@yhvyc8b)vn!#2fzk=b5ouv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unclesstudioworld.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'static',
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
