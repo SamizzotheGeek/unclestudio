6f45250451647b9c35b9107e15ee35e661d93319
Samizzo <samizzographics@gmail.com>
2020-04-16 11:39:11 +0300

    editing team management

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ class Employees(models.Model):
     position = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
     short_bio = models.CharField(max_length=200)
-    profile = models.ImageField(upload_to='images/')
+    profile = models.ImageField(upload_to='images/', default='empty.jpg') 
 
     def __str__(self):
         return self.name
